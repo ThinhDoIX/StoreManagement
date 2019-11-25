775f3f06d8bde7e27e5565522f1ba9b421342ea3
Thinh <doduythinh.1999@gmail.com>
2019-11-25 12:43:22 +0700

    Add label maNV

--- StoreManager/GUI/EmployeeForm.Designer.cs
+++ StoreManager/GUI/EmployeeForm.Designer.cs
@@ -60,6 +60,7 @@
             this.clchucvu = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label1 = new System.Windows.Forms.Label();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.lbl_maNV = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel4.SuspendLayout();
@@ -80,6 +81,7 @@
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel2.Controls.Add(this.lbl_maNV);
             this.panel2.Controls.Add(this.panel4);
             this.panel2.Controls.Add(this.pictureBox1);
             this.panel2.Location = new System.Drawing.Point(3, 134);
@@ -362,6 +364,16 @@
             this.panel3.Size = new System.Drawing.Size(349, 150);
             this.panel3.TabIndex = 6;
             // 
+            // lbl_maNV
+            // 
+            this.lbl_maNV.AutoSize = true;
+            this.lbl_maNV.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_maNV.Location = new System.Drawing.Point(269, 145);
+            this.lbl_maNV.Name = "lbl_maNV";
+            this.lbl_maNV.Size = new System.Drawing.Size(63, 29);
+            this.lbl_maNV.TabIndex = 7;
+            this.lbl_maNV.Text = "nv01";
+            // 
             // EmployeeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -382,6 +394,7 @@
             this.Load += new System.EventHandler(this.EmployeeForm_Load);
             this.panel1.ResumeLayout(false);
             this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.panel4.ResumeLayout(false);
             this.panel4.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -427,5 +440,6 @@
         private System.Windows.Forms.Label lbl_sodiennthoai;
         private System.Windows.Forms.DateTimePicker dtp_ngaysinh;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lbl_maNV;
     }
 }
